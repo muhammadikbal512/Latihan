d383774f0c6a079d1b3bbab2e4a4b1522250281d
muhammadikbal512 <muhammadikbal511@gmail.com>
2021-01-25 14:10:35 +0700

    http axios

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Blog from './containers/Blog/Blog';
 
 class App extends Component {
   render() {
-    this.componentDidCatch
+    
     return (
       <div className="App">
         <Blog />
